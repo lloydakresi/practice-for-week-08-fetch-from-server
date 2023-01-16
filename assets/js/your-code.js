d623cc8cb319f8849ff5f0b6c17f1f1a730c6d3e
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-16 23:20:46 +0000

    completed

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,54 @@
 export function getAllDogs() {
     // Your code here
+    return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    return fetch("/dogs/2")
 }
 
 export function postNewDog() {
     // Your code here
+    let url = "/dogs";
+    let header = {"Content-Type": "application/x-www-form-urlencoded"};
+    let body = new URLSearchParams({
+        name: "Dexter",
+        age: 11
+})
+
+    return fetch(url, {
+        method: "POST",
+        headers: header,
+        body: body
+    });
 }
 
 export function postNewDogV2(name, age) {
      // Your code here
+     let url = "/dogs";
+    let header = {"Content-Type": "application/x-www-form-urlencoded"};
+    let body = new URLSearchParams({
+        name: name,
+        age: age
+    })
+
+    return fetch(url, {
+        method: "POST",
+        headers: header,
+        body: body
+    });
 }
 
 export function deleteDog(id) {
       // Your code here
+      let url = `/dogs/${id}/delete`;
+      let header = {AUTH: "ckyut5wau0000jyv5bsrud90y"}
+      let body = "";
+
+      return fetch(url, {
+        method: "POST",
+        headers: header,
+        body: body
+      })
 }
